e3f1d35367cd566d283dec2bd2501a661a841e33
Arseny116 <Olifirov24@gmail.com>
2025-08-01 18:22:08 +0300

    Add Pet models

---
+++ backend/PetTrackBack/PetTrackBack.Core/Models/Pet.cs
@@ -0,0 +1,39 @@
+﻿namespace PetTrackBack.Core.Models
+{
+    public class Pet
+    {
+        const int LengthName = 100;
+        public Guid Id { get; }
+        public string Name { get; } = string.Empty;
+
+        public string Species { get; } = string.Empty;
+
+
+        public string DateOfBirth { get; } = string.Empty;
+
+
+        private Pet(Guid id, string name, string species, string dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Species = species;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public static (Pet, string) Create(Guid id, string name, string species, string dateOfBirth)
+        {
+            var error = string.Empty;
+            if (string.IsNullOrEmpty(name) || (name.Length >= 100))
+            {
+                error = "Error is length";
+            }
+
+            Pet pet = new Pet(id, name, species, dateOfBirth);
+
+
+            return (pet, error);
+        }
+
+
+    }
+}
